e3b5a33b23efc6679f058f3215bab7790e0941bb
Patrick Dubroy <pdubroy@gmail.com>
2016-01-28 16:32:28 +0100

    self become: #ai-to-ns

--- README.md
+++ README.md
@@ -1,38 +1,12 @@
-cloud-to-butt
-=============
+ai-to-ns
+========
 
-![](logo.png)
+"Artificial Intelligence is no match for natural stupidity" -Alan Kay
 
-Chrome extension that replaces occurrences of 'the cloud' with 'my butt'
-
-[Direct download of crx file](https://github.com/panicsteve/cloud-to-butt/blob/master/CloudToButt.crx?raw=true)
-
-Note that there are forks of this extension that simply replace 'cloud' with 'butt'.
-In my personal opinion, that approach is too broad and it's less funny as a result, but it is clearly a very
-polarizing issue in the cloud-to-butt user community.  Forks are free to do whatever they like.  But officially, _this_ extension replaces only the phrase described above. Thank you for your concern.
-
-Screenshot Gallery
-------------------
-
-http://www.flickr.com/groups/cloud-to-butt/
+Chrome extension that replaces occurrences of 'artificial intelligence' with 'natural stupidity'.
+Based on Steven Frank's "cloud-to-butt" extension: https://github.com/panicsteve/cloud-to-butt
 
 Installation
 ------------
 
-In Chrome, choose Window > Extensions.  Drag CloudToButt.crx into the page that appears.
-
-Safari Version
---------------
-
-Can be found here: https://github.com/logancollins/cloud-to-butt-safari
-
-Firefox Version
----------------
-
-Can be found here: https://github.com/DaveRandom/cloud-to-butt-mozilla
-
-
-Opera Version
----------------
-
-Can be found here: https://github.com/DaveRandom/cloud-to-butt-opera
+Install it directly from the [Chrome web store](https://chrome.google.com/webstore/detail/ai-to-ns/eigdhdcehddpggeegcaefieeejcknoom).
--- Source/content_script.js
+++ Source/content_script.js
@@ -1,42 +1,33 @@
 walk(document.body);
 
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
+function walk(node)  {
+  var child, next;
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+  switch (node.nodeType) {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+    case 3:  // Text node
+      handleText(node);
+      break;
+  }
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
-
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+function handleText(textNode) {
+  textNode.nodeValue = textNode.nodeValue
+      .replace(/\ban AI\b/g, 'Natural Stupidity')
+      .replace(/\bAIs\b/g, 'Natural Stupidities')
+      .replace(/\bA.I.s\b/g, 'Natural Stupidities')
+      .replace(/\bAI\b/g, 'Natural Stupidity')
+      .replace(/\bA.I.\b/g, 'Natural Stupidity')
+      .replace(/\bArtificial Intelligence\b/g, 'Natural Stupidity')
+      .replace(/\bArtificial intelligence\b/g, 'Natural stupidity')
+      .replace(/\bartificial intelligence\b/g, 'Natural Stupidity');
 }
-
-
